ec7fcb81d3f68ab449ef0614765725862e13cc09
Sota Yokoe <kreuz45@kreuz45.com>
2017-03-11 12:18:25 +0900

    Support color schemes

--- PlaybackSlider/PlaybackSlider.swift
+++ PlaybackSlider/PlaybackSlider.swift
@@ -7,20 +7,41 @@ public class PlaybackSlider: NSSlider {
             setNeedsDisplay()
         }
     }
+    
+    public var contentBackgroundColor: NSColor = NSColor.darkGray {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    public var knobColor: NSColor = NSColor.windowBackgroundColor {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    public var knobBorderColor: NSColor = NSColor.black {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    public var cachedSegmentColor: NSColor = NSColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 1.0) {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
 
     let bezelMargin: CGFloat = 4
     
     override public func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
         
-        NSColor.darkGray.setFill()
+        contentBackgroundColor.setFill()
         let bezelFrame = bounds.insetBy(dx: bezelMargin, dy: bezelMargin)
         NSBezierPath(roundedRect: bezelFrame, xRadius: bezelFrame.height * 0.5, yRadius: bezelFrame.height * 0.5).fill()
         
         let innerRect = bounds.insetBy(dx: bounds.height / 2, dy: 0)
         
         // Cached segments
-        NSColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 1.0).setFill()
+        cachedSegmentColor.setFill()
         cachedSegments?.forEach { fragment in
             let start = CGFloat(fragment.start) / CGFloat(maxValue + 1)
             let end = CGFloat(fragment.end + 1) / CGFloat(maxValue + 1)
@@ -29,8 +50,7 @@ public class PlaybackSlider: NSSlider {
         }
         
         //Knob
-        
-        NSColor.windowBackgroundColor.setFill()
+        knobColor.setFill()
         let knobX: CGFloat
         if maxValue - minValue == 0 {
             knobX = innerRect.minX
@@ -40,7 +60,7 @@ public class PlaybackSlider: NSSlider {
         let knobPath = NSBezierPath(ovalIn: NSRect(x: knobX - bounds.height * 0.5, y: 0, width: bounds.height, height: bounds.height).insetBy(dx: 2, dy: 2))
         knobPath.fill()
         
-        NSColor.black.setStroke()
+        knobBorderColor.setStroke()
         knobPath.lineWidth = 2
         knobPath.stroke()
         
--- PlaybackSliderDev/ViewController.swift
+++ PlaybackSliderDev/ViewController.swift
@@ -3,6 +3,7 @@ import PlaybackSlider
 
 class ViewController: NSViewController {
     @IBOutlet weak var slider: PlaybackSlider!
+    @IBOutlet weak var coloredSlider: PlaybackSlider!
     @IBOutlet weak var someView: NSView!
 
     override func viewDidLoad() {
@@ -10,13 +11,20 @@ class ViewController: NSViewController {
         
         someView.wantsLayer = true
         someView.layer?.backgroundColor = NSColor.white.cgColor
+        
+        coloredSlider.contentBackgroundColor = NSColor.blue
+        coloredSlider.knobColor = NSColor.red
+        coloredSlider.knobBorderColor = NSColor.white
+        coloredSlider.cachedSegmentColor = NSColor.yellow
     }
     
     @IBAction func onRandomizeButton(_ sender: Any) {
         var segments = [CachedSegment]()
         segments.append(CachedSegment(start: 0, end: 25))
         segments.append(CachedSegment(start: 50, end: 75))
-        slider.cachedSegments = segments
+        [slider, coloredSlider].forEach { (slider) in
+            slider.cachedSegments = segments
+        }
     }
 
     @IBAction func onZeroRange(_ sender: Any) {
