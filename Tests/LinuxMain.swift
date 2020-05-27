044a98cc5d13e12b8bc7231365f256e12c0c988b
Sota Yokoe <sota@kreuz45.com>
2020-05-27 20:07:46 +0900

    chore: Support SPM

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import PlaybackSliderTests
+
+var tests = [XCTestCaseEntry]()
+tests += PlaybackSliderTests.allTests()
+XCTMain(tests)
---
+++ Tests/PlaybackSliderTests/PlaybackSliderTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import PlaybackSlider
+
+final class PlaybackSliderTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(PlaybackSlider().text, "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
